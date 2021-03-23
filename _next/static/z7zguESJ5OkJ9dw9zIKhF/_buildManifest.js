70b14aa9b438ef85ca368e1e67d743e1ec6c0f58
hamatatsu <hamatatsu@users.noreply.github.com>
2021-03-23 05:19:22 +0000

    deploy: aad203c2b8a7ca522254405fbbec17826c14f4c8

---
+++ _next/static/z7zguESJ5OkJ9dw9zIKhF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Ff8444f32bae8419444f5.css","static\u002Fchunks\u002Fpages\u002Findex-e57257fc221ea3bed546.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-df79953ec310c67fe816.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
